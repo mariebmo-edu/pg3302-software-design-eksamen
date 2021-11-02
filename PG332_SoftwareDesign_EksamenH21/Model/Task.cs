0e26d9eb3f89a97ba53f268ac7a2c76ced57607a
Christian <chgr007@kristiania.no>
2021-11-02 11:29:30 +0100

    Added Id props, semester and course

--- PG332_SoftwareDesign_EksamenH21/Model/Task.cs
+++ PG332_SoftwareDesign_EksamenH21/Model/Task.cs
@@ -1,8 +1,11 @@
+using System;
+
 namespace PG332_SoftwareDesign_EksamenH21
 {
     public class Task
     {
-        public long TaskId { get; set; }
+        
+        public long Id { get; set; }
         public long LectureId { get; set; }
         public Lecture Lecture { get; set; }
         public IProgression Progression { get; set; }
