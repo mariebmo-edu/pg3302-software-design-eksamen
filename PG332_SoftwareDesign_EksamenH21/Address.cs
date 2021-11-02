89e527f3f903ad9e4d74da2ce23cee76ec97d8cc
harryapp <harry.mliam@gmail.com>
2021-11-02 11:32:30 +0100

    add id and address

---
+++ PG332_SoftwareDesign_EksamenH21/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace PG332_SoftwareDesign_EksamenH21
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string StreetNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
