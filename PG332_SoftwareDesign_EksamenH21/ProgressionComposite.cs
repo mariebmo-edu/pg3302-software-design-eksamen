5b8cd59200e193e1885c8361c58ce154b3416e69
Generoman <roman181@gmail.com>
2021-11-02 13:39:43 +0100

    Implemented ProgressionNode & ProgressionComposite

--- PG332_SoftwareDesign_EksamenH21/ProgressionComposite.cs
+++ PG332_SoftwareDesign_EksamenH21/ProgressionComposite.cs
@@ -7,16 +7,42 @@ namespace PG332_SoftwareDesign_EksamenH21
         public double Total { get; set; }
         public bool Published { get; set; }
         public double Finished { get; set; }
-        public List<IProgression> Children { get; set; }
+        public Array<IProgression> Children { get; set; }
 
-        public List<IProgression> ListChildren()
+        public Array<IProgression> ListChildren()
         {
             return null;
         }
 
         public double GetProgression()
         {
-            throw new System.NotImplementedException();
+            double returnValue = 0.00;
+
+            if (published)
+            {
+                for (IProgression Child :
+                Children)
+                {
+                    if (Child.Published)
+                    {
+                        returnValue = returnValue + Child.GetProgression();
+                    }
+                }
+            }
+
+            return returnValue / Total;
+        }
+
+        public ProgressionComposite(int TotalChildren)
+        {
+            if (TotalChildren < 1)
+            {
+                throw new ArgumentOutOfRangeException("TotalChildren must be over 0");
+            }
+            Total = TotalChildren;
+            Published = false;
+            Finished = 0.00;
+            Children = new IProgression[TotalChildren];
         }
     }
 }
