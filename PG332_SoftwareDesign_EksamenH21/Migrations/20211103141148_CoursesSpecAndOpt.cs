4d0bcf315c5530d8b8ae4adb7916e617f0c9aaca
Christian <chgr007@kristiania.no>
2021-11-03 15:16:31 +0100

    Added new migration

---
+++ PG332_SoftwareDesign_EksamenH21/Migrations/20211103141148_CoursesSpecAndOpt.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PG332_SoftwareDesign_EksamenH21.Migrations
+{
+    public partial class CoursesSpecAndOpt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OptionalCourses",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<long>(type: "INTEGER", nullable: false),
+                    CourseId = table.Column<long>(type: "INTEGER", nullable: false),
+                    SemesterEnum = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OptionalCourses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SpecializationCourses",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SpecializationId = table.Column<long>(type: "INTEGER", nullable: false),
+                    CourseId = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpecializationCourses", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OptionalCourses");
+
+            migrationBuilder.DropTable(
+                name: "SpecializationCourses");
+        }
+    }
+}
