477d3c78cbf2eaf9ed2fc1b82ad241007e32ee3e
Christoffer <links685@gmail.com>
2021-11-07 00:44:06 +0100

    ShouldLoginUser() 🟢 - need UI imp to get UserCredentials correct

---
+++ PG332_SoftwareDesign_EksamenH21/BusinessLogic.cs
@@ -0,0 +1,57 @@
+﻿using PG332_SoftwareDesign_EksamenH21.Model;
+using PG332_SoftwareDesign_EksamenH21.Repository;
+
+namespace PG332_SoftwareDesign_EksamenH21
+{
+    public class BusinessLogic
+    {
+        private IUserInterface UI { get; } = new UserInterface();
+        private User User;
+        
+        
+        public BusinessLogic(IUserInterface ui)
+        {
+            UI = ui;
+        }
+
+        public void StartProgram() 
+        {
+            User = UI.GetLoginCredentials(); // Må først få bruker til å logge inn
+            while (!UserValid(User))
+            {
+                User = UI.GetLoginCredentials(); // Så lenge bruker ikke er gyldig ber vi dem prøve igjen.
+            }
+            ExecuteUserChoice();
+        }
+
+        private bool UserValid(User user)
+        {
+            UserDao dao = new UserDao();
+            User actual = dao.RetrieveByEmail(user.Email);
+            // TODO: Sjekke for passord her??
+            return user.Id == actual.Id;
+        }
+
+        private void ExecuteUserChoice()
+        {
+            UI.ShowMenuOptions();
+            UserChoice userChoice = UI.GetUserChoice();
+
+            switch (userChoice)
+            {
+                case UserChoice.GetSemesterProgression:
+                {
+                    if (User.SpecializationId != null)
+                        new SpecializationDao().RetrieveById((long) User.SpecializationId);
+                    break;
+                }
+            }
+        }
+
+        public User GetLoggedInUser()
+        {
+            return User;
+        }
+    }
+    
+}
