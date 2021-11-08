5e8472ea063a921fe6ecc8bec9acea37b6a56b71
Generoman <roman181@gmail.com>
2021-11-08 17:35:40 +0100

    Changes to accommodate pull request

--- PG332_SoftwareDesign_EksamenH21/Model/Course.cs
+++ PG332_SoftwareDesign_EksamenH21/Model/Course.cs
@@ -5,7 +5,7 @@ using PG332_SoftwareDesign_EksamenH21.Model;
 
 namespace PG332_SoftwareDesign_EksamenH21
 {
-    public class Course
+    public class Course : IProgressable
     {
         [NotMapped]
         public SemesterEnum Semester { get; set; }
@@ -15,8 +15,7 @@ namespace PG332_SoftwareDesign_EksamenH21
         public DateTime ExamDate { get; set; }
         public ExamType ExamType { get; set; }
         public float CoursePoints { get; set; }
-        [NotMapped]
-        public IProgression Progression { get; set; }
+        [NotMapped] public bool Published { get; set; } = false;
 
     }
 
--- PG332_SoftwareDesign_EksamenH21/Model/Task.cs
+++ PG332_SoftwareDesign_EksamenH21/Model/Task.cs
@@ -3,11 +3,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PG332_SoftwareDesign_EksamenH21
 {
-    public class Task
+    public class Task : IFinishable
     {
         public long Id { get; set; }
         public string Name { get; set; }
-        [NotMapped]
-        public IProgression Progression { get; set; }
+        [NotMapped] public bool Published { get; set; } = false;
+        [NotMapped] public bool Finished { get; set; } = false;
     }
 }
