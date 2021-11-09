fccd4e4be2f7bfbdcd27dd13142a4abe35ef45f8
harryapp <harry.mliam@gmail.com>
2021-11-09 10:34:43 +0100

    add IUserInterface

---
+++ PG332_SoftwareDesign_EksamenH21/IUserInterface.cs
@@ -0,0 +1,19 @@
+﻿namespace PG332_SoftwareDesign_EksamenH21
+{
+    public interface IUserInterface
+    {
+        void PrintMessage(string message);
+
+        void PrintPercentageDone(int percentage);
+
+        void ShowMainMenu();
+
+        void ShowSubMenu();
+
+        void ShowRegisterUserMenu();
+
+        void ShowSelectSpecializationMenu();
+
+        void ShowSelectOptionalCourseMenu();
+    }
+}
