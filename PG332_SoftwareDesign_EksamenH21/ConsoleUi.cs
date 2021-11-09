65a96680b3df6cb35af8b15a5bb463b04021eeec
harryapp <harry.mliam@gmail.com>
2021-11-09 11:34:05 +0100

    add ConsoleUi. Methods not implemented yet (implements IConsoleUi)

---
+++ PG332_SoftwareDesign_EksamenH21/ConsoleUi.cs
@@ -0,0 +1,40 @@
+﻿namespace PG332_SoftwareDesign_EksamenH21
+{
+    public class ConsoleUi : IConsoleUi 
+    {
+        public void PrintMessage(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PrintPercentageDone(int percentage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ShowMainMenu()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ShowSubMenu()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ShowRegisterUserMenu()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ShowSelectSpecializationMenu()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ShowSelectOptionalCourseMenu()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
