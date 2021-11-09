1ae93478624ac5f6fcf4051c15b1f8b6a601ba02
harryapp <harry.mliam@gmail.com>
2021-11-09 11:55:25 +0100

    ShouldPrintToConsole(fail)

---
+++ Test/ConsoleUiTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using PG332_SoftwareDesign_EksamenH21;
+
+namespace Test
+{
+    public class ConsoleUiTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        [TestCase("Hello") ]
+        [TestCase("Dette er en tekst")]
+        public void ShouldPrintToConsole(string input)
+        {
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            
+            ConsoleUi consoleUi = new ConsoleUi();
+            consoleUi.PrintMessage(input);
+            
+            Assert.AreEqual(input + "\r\n", stringWriter.ToString());
+        }
+
+    }
+}
