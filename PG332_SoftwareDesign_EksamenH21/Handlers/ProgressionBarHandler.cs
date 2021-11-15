5032bed54d439723d85e68dbc28e1a808970505b
harryapp <harry.mliam@gmail.com>
2021-11-15 16:55:54 +0100

    add ProgressionBarHandler.cs and dummyTest

---
+++ PG332_SoftwareDesign_EksamenH21/Handlers/ProgressionBarHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using PG332_SoftwareDesign_EksamenH21.Handlers;
+
+namespace PG332_SoftwareDesign_EksamenH21
+{
+    public class ProgressionBarHandler
+    {
+        public static String GenereteProgressbar(ProgressionWrapper progressionWrapper)
+        {
+            String sample = "|####################==========----------|";
+
+
+            double finishedLength = 100 * progressionWrapper.FinishedPercent;
+            double publishedLength = (100 * progressionWrapper.PublishedPercent) - finishedLength;
+            
+            String buffer = "|";
+            buffer += barGenerator('#', Convert.ToInt32(finishedLength));
+            buffer += barGenerator('=', Convert.ToInt32(publishedLength));
+            buffer += barGenerator('-', 100 - buffer.Length - 1);
+
+            return buffer;
+        }
+
+        public static String barGenerator(char c, int amount)
+        {
+            StringBuilder buffer = new();
+            for (int i = 0; i < amount; i++)
+            {
+                buffer.Append(c);
+            }
+            return buffer.ToString();
+        }
+    }
+}
---
+++ Test/ProgressionBarHandlerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using PG332_SoftwareDesign_EksamenH21;
+using PG332_SoftwareDesign_EksamenH21.Handlers;
+
+namespace Test
+{
+    public class ProgressionBarHandlerTest
+    {
+        [Test]
+        public void ShouldGenerateBar()
+        {
+            ProgressionWrapper progressionWrapper = new(0.66, 0.25);
+
+            Console.WriteLine(ProgressionBarHandler.GenereteProgressbar(progressionWrapper));
+            
+            Assert.True(true);
+
+        }
+    }
+}
