76d8fd891456806f8662faac0ad8b0b80ab51ef6
harryapp <harry.mliam@gmail.com>
2021-11-15 15:40:30 +0100

    f

---
+++ PG332_SoftwareDesign_EksamenH21/Model/StudentLecture.cs
@@ -0,0 +1,11 @@
+﻿namespace PG332_SoftwareDesign_EksamenH21.Model
+{
+    public class StudentLecture
+    {
+        public long Id { get; set; }
+        public long LectureId { get; set; }
+        public long StudentId { get; set; }
+        public bool LectureSeen { get; set; }
+        
+    }
+}
