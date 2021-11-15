95e4370a89f7855c76522b7a105119cab3df8f53
harryapp <harry.mliam@gmail.com>
2021-11-15 20:50:44 +0100

    add

--- PG332_SoftwareDesign_EksamenH21/ConsoleUi.cs
+++ PG332_SoftwareDesign_EksamenH21/ConsoleUi.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using PG332_SoftwareDesign_EksamenH21.Controllers;
 using PG332_SoftwareDesign_EksamenH21.Handlers;
 using PG332_SoftwareDesign_EksamenH21.Model;
 using PG332_SoftwareDesign_EksamenH21.Repository;
@@ -44,6 +45,9 @@ namespace PG332_SoftwareDesign_EksamenH21
         {
             // Email = "gMail@email.no", password = HashPassword("password123")};
             UserLoginMenu();
+            MainMenuController menuController = new();
+            menuController.Handle(User.Id);
+            
             string firstName = User.FirstName + " " + User.LastName;
             while (true)
             {
