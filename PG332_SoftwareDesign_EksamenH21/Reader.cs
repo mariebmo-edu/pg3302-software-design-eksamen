104436b050381b61a2cfd87fffa4bdae730502b8
Christoffer <links685@gmail.com>
2021-11-17 21:55:29 +0100

    small updates

--- PG332_SoftwareDesign_EksamenH21/Reader.cs
+++ PG332_SoftwareDesign_EksamenH21/Reader.cs
@@ -1,7 +1,12 @@
-﻿namespace PG332_SoftwareDesign_EksamenH21
+﻿using System;
+
+namespace PG332_SoftwareDesign_EksamenH21
 {
     public class Reader : IReader
     {
-        
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
     }
 }
