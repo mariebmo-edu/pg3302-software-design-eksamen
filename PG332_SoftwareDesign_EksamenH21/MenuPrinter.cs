462ccd0be7be4b8151b31cb22c426c287680c40d
Christoffer <links685@gmail.com>
2021-11-17 15:27:02 +0100

    small updates

--- PG332_SoftwareDesign_EksamenH21/MenuPrinter.cs
+++ PG332_SoftwareDesign_EksamenH21/MenuPrinter.cs
@@ -8,48 +8,45 @@ namespace PG332_SoftwareDesign_EksamenH21
 {
     public class MenuPrinter<T>
     {
-        public void ShowMenu(IPrintable<T> printable)
+        public void ShowMenu(OptionsHandler oh)
         {
-            if (printable is OptionsHandler)
+
+            Console.WriteLine(oh.Progressable.Title);
+
+            IProgressionHandler<IProgressable> ph =
+                ProgressionHandlerFactory.MakeProgressionHandler(oh.Progressable);
+            ProgressionWrapper pw = ph.GetProgression();
+            Console.WriteLine(ProgressionBarHandler.GenerateProgressBar(pw));
+
+            StringBuilder menuOptionsString = new();
+
+            if (oh.IsFinishable)
             {
-                OptionsHandler oh = printable as OptionsHandler;
-
-                Console.WriteLine(oh.Progressable.Title);
-
-                IProgressionHandler<IProgressable> ph =
-                    ProgressionHandlerFactory.MakeProgressionHandler(oh.Progressable);
-                ProgressionWrapper pw = ph.GetProgression();
-                Console.WriteLine(ProgressionBarHandler.GenerateProgressBar(pw));
-
-                StringBuilder menuOptionsString = new();
-
-                if (oh.IsFinishable)
+                IFinishable f = oh.Progressable as IFinishable;
+                menuOptionsString.Append("[F] - Sett som ");
+                if (!f.Finished)
                 {
-                    IFinishable f = oh.Progressable as IFinishable;
-                    menuOptionsString.Append("[F] - Sett som ");
-                    if (!f.Finished)
-                    {
-                        menuOptionsString.Append("ferdig\n");
-                    }
-                    else
-                    {
-                        menuOptionsString.Append("uferdig\n");
-                    }
+                    menuOptionsString.Append("ferdig\n");
                 }
-
-                for (int i = 0; i < oh.Options.Count; i++)
+                else
                 {
-                    menuOptionsString.Append("[" + (i + 1) + "] - ");
-                    menuOptionsString.Append(oh.Options[i].Title + "\n");
+                    menuOptionsString.Append("uferdig\n");
                 }
-
-                if (oh.SuperOption != null)
-                {
-                    menuOptionsString.Append("\n[0] - Tilbake til " + oh.SuperOption.Progressable.Title);
-                }
-
-                Console.WriteLine(menuOptionsString.ToString());
             }
+
+            for (int i = 0; i < oh.Options.Count; i++)
+            {
+                menuOptionsString.Append("[" + (i + 1) + "] - ");
+                menuOptionsString.Append(oh.Options[i].Title + "\n");
+            }
+
+            if (oh.SuperOption != null)
+            {
+                menuOptionsString.Append("\n[0] - Tilbake til " + oh.SuperOption.Progressable.Title);
+            }
+
+            Console.WriteLine(menuOptionsString.ToString());
+
             Console.WriteLine("\n[E] - Avslutt");
         }
 
