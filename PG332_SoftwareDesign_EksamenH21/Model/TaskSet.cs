cc75faefc2ea95d3564a91f1673025fb550d9a97
Christian Gregersen <chgr007@student.kristiania.no>
2021-11-18 14:27:23 +0100

    fixed LectureID in TaskSet

--- PG332_SoftwareDesign_EksamenH21/Model/TaskSet.cs
+++ PG332_SoftwareDesign_EksamenH21/Model/TaskSet.cs
@@ -7,7 +7,8 @@ namespace PG332_SoftwareDesign_EksamenH21
     public class TaskSet : IProgressable
     {
         public int Id { get; set; }
-        public long LectureId { get; private set; }
+        public Lecture Lecture { get; set; }
+        public int LectureId { get; private set; }
         public List<Task> Tasks { get; set; }
         [NotMapped] public bool Published { get; set; } = true;
         [NotMapped] public string Title { get; } = "Oppgavesett";
