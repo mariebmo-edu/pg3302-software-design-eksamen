ac0ef73c6f8b7fd5a15f5d662396f6013a28c0d0
Christoffer <links685@gmail.com>
2021-11-18 21:12:41 +0100

    removed unnecessary test

--- Test/ConsoleUiTest.cs
+++ Test/ConsoleUiTest.cs
@@ -27,35 +27,5 @@ namespace Test
 
             Assert.AreEqual(input + "\r\n", stringWriter.ToString());
         }
-
-        [Test]
-        public void ShouldPrintMainMenu()
-        {
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-            
-            ConsoleUi consoleUi = new ConsoleUi();
-            consoleUi.ShowMainMenu();
-            
-            // Expected:
-            string name = "Harry";
-            string semester = "3";
-            string progressionBar = "|####################==========----------|";
-            string expected = $"Velkommen, {name}\r\n" +
-                              $"Nåværende semester: {semester}\r\n" +
-                              $"{progressionBar}\r\n" +
-                              "\r\n" +
-                              $"Velg emne:\r\n" +
-                              $"1 - AdvJava\r\n" +
-                              $"2 - SoftDes\r\n" +
-                              $"3 - AlgDat\r\n" +
-                              $"4 - SmiPro\r\n" +
-                              "\r\n" +
-                              $"0 - gå til spesialiseringsmeny\r\n" +
-                              "\r\n" +
-                              "E - avslutt\r\n";
-            Assert.AreEqual(expected, stringWriter);
-        }
-
     }
 }
